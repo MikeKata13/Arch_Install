c88d6290695c972981574a20997ca3ec7f9c7c73
MikeKata <michaliskatarachias@gmail.com>
2022-02-10 04:07:43 +0200

    Add files via upload

--- required_packages.sh
+++ required_packages.sh
@@ -11,10 +11,16 @@ makepkg -si
 yes | paru -Syu
 
 #Install needed packages
-sudo pacman -S firefox alacritty lxappearance gnome-tweaks qtile zsh rofi neofetch 
-paru -S brave-bin
+sudo pacman -S xorg-server pulseaudio xfce4 xfce4-goodies firefox alacritty lxappearance gnome-tweaks qtile zsh rofi neofetch nemo libreoffice-fresh vlc code
+
+paru -S brave-bin spotify
+
 
 #Create a Qtile config file
 mkdir ~/.config/qtile
+#Here clone and copy your custom qtile configuration
 sudo cp usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
 sudo chown mike:root ~/.config/qtile/config.py
+
+#Finish
+echo "Script Completed Successfully"
