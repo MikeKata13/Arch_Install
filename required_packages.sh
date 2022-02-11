b26a48b6d64eab596e5273a4d628ffd59a6cc735
MikeKata <michaliskatarachias@gmail.com>
2022-02-11 16:53:34 +0200

    Add files via upload

--- required_packages.sh
+++ required_packages.sh
@@ -11,16 +11,25 @@ makepkg -si
 yes | paru -Syu
 
 #Install needed packages
-sudo pacman -S xorg-server pulseaudio xfce4 xfce4-goodies firefox alacritty lxappearance gnome-tweaks qtile zsh rofi neofetch nemo libreoffice-fresh vlc code
+sudo pacman -S xorg-server pulseaudio xfce4 firefox alacritty lxappearance nitrogen picom gnome-tweaks qtile zsh rofi neofetch nemo libreoffice-fresh vlc code
+sddm python-psutil papirus-icon-theme geany
+paru -S brave-bin spotify nordic-theme flat-remix
 
-paru -S brave-bin spotify
-
+#Enable Display Manager
+sudo systemctl enable sddm
 
 #Create a Qtile config file
 mkdir ~/.config/qtile
 #Here clone and copy your custom qtile configuration
-sudo cp usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
+sudo cp /usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
 sudo chown mike:root ~/.config/qtile/config.py
+git clone https://github.com/MikeKata13/Qtile.git
+cd Qtile
+cp config.py ~/.config/qtile/config.py
+
+
+#Create a picom configurration file
+sudo cp /etc/xdg/picom.conf ~/.config/
 
 #Finish
 echo "Script Completed Successfully"
